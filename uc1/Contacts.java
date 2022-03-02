8b2951901aca3403a6adc859e34a045b9d6b8265
prakashudikeri <pu010889@gmail.com>
2022-03-02 21:32:21 +0530

    UC1 to UC4 push

---
+++ uc1/Contacts.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.uc1;
+
+public class Contacts {
+    String firstName;							//Variable Declaration for Contacts Class
+    String lastname;
+    String address;
+    String city;
+    String state;
+    String zip;
+    String phoneNo;
+    String emailID;
+}
